cb5f3f35bb7792b185442a5fb3ebcfe3a36c68c5
Cam <cwashington@mail.sfsu.edu>
2021-07-19 15:18:13 -0700

    Rudimentary header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
   return (
-    <div className="App">
+    <>
 
-      <h1> Turning Heads...</h1>
+      <Header />
+      <main>
+          <Container>
+          <h1> Turning Heads...</h1>
+          </Container>
+        
+      
+      </main>
+      <Footer/>
 
-    </div>
+    </>
   );
 }
 
